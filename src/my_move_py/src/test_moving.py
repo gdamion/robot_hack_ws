4776422183f763a99f0becd40073676695399984
slaveareyou <addlesssugar@gmail.com>
2019-09-28 02:19:16 +0300

    py

---
+++ src/my_move_py/src/test_moving.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# include <ros/ros.h>
+# include <math.h>
+# include <geometry_msgs/Twist.h>
+import rospy
+import geometry_msgs.Twist
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
