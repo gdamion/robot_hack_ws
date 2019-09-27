a3e049d6107e95730853d9cb31c2ef2cf3082cd5
slaveareyou <addlesssugar@gmail.com>
2019-09-27 21:06:36 +0300

    ss

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/al/Code/robot_hack_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/al/Code/robot_hack_ws/devel;/opt/ros/melodic".split(';') if "/home/al/Code/robot_hack_ws/devel;/opt/ros/melodic" != "" else []
--- build/my_move_py/CMakeFiles/move_node_py.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for move_node_py.
-# This may be replaced when dependencies are built.
